bf30080b336d24b907bfaf7816dbe1cc137bb062
Daniela Rubis <danielarubis@hotmail.com>
2022-02-14 11:00:51 -0300

    We add styles to the form and table to visualize the data saved in the database and we add a confirmation form with js

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Quentin123' #cambiar a palabra reservada 'PASSWORD'
+app.config['MYSQL_PASSWORD'] = 'Quentin123'
 app.config['MYSQL_DB'] = 'flaskcontacts' #database
 mysql = MySQL(app)
 
